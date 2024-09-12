e8cbf887604617f69520d42ca8d8d141605922ce
FluffyChi-Xing <121754413+FluffyChi-Xing@users.noreply.github.com>
2024-09-12 11:50:37 +0800

    个人中心页暂提

---
+++ src/views/MinePage/_components/UserAvatar.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  userAvatar?:string;
+  username?: string;
+}>(), {
+  userAvatar: 'https://picsum.photos/200/300?2',
+  username: '用户名'
+})
+</script>
+
+<template>
+  <div class="w-full h-full flex px-5">
+    <div class="w-16 h-full flex rounded-[32px] overflow-hidden">
+      <img :src="userAvatar" alt="" loading="lazy" class="w-full h-full flex">
+    </div>
+    <div class="w-auto ml-2 h-full max-w-1/2 flex items-center">
+      <span class="align-middle w-auto h-auto text-white text-xl font-bold text-ellipsis overflow-hidden whitespace-nowrap">{{ username }}</span>
+    </div>
+    <div class="w-auto h-full flex ml-auto items-center">
+      <span class="text-white">个人信息</span>
+      <van-icon name="arrow" color="#fff" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/MinePage/index.vue
+++ src/views/MinePage/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import type {UserTypes} from "@/conponsables/api/UserTypes";
+import UserAvatar from "@/views/MinePage/_components/UserAvatar.vue";
 
 
 /** ===== 用户信息-start ===== **/
@@ -23,15 +24,44 @@ const defaultList = [
   }
 ]
 const userInfo = ref<UserTypes.UserInfoTypes[]>(defaultList)
+const username = ref<string>('admin')
 /** ===== 用户信息-end ===== **/
+
+/** ===== 卡片栏A-start ===== **/
+interface cardAtypes {
+  label: string;
+  icon: string;
+  url?: string;
+}
+const cardAList = ref<cardAtypes[]>([
+  {
+    label: '消息通知',
+    icon: 'bell'
+  },
+  {
+    label: '我的收藏',
+    icon: 'gift-o'
+  },
+  {
+    label: '浏览历史',
+    icon: 'clock-o'
+  },
+  {
+    label: '我的作品',
+    icon: 'notes-o'
+  }
+])
+/** ===== 卡片栏A-end ===== **/
 </script>
 
 <template>
   <div class="w-full h-full flex flex-col relative">
     <div class="w-full h-1/3 top-bg flex bg-gradient-to-b from-[#53517F] to-[#53517F80] absolute top-0" />
     <!-- user avatar -->
-    <div class="w-full h-16 flex absolute top-[20px] bg-red-500 z-[19]">
-
+    <div class="w-full h-16 flex absolute top-[20px] z-[19]">
+      <UserAvatar
+          :username="username"
+      />
     </div>
     <!-- user info -->
     <div class="w-full h-16 flex justify-center absolute top-[100px] z-[19]">
@@ -48,11 +78,46 @@ const userInfo = ref<UserTypes.UserInfoTypes[]>(defaultList)
     </div>
     <!-- 卡片A -->
     <div class="w-full h-20 flex justify-center absolute px-5 top-[190px] z-[19]">
-      <div class="w-full h-full info-card flex" />
+      <div class="w-full h-full info-card grid grid-cols-4 gap-2">
+        <div
+            v-for="(item, index) in cardAList"
+            :key="index"
+            class="w-full h-full flex px-4"
+        >
+          <div class="w-full h-full py-4 flex flex-col items-center">
+            <div class="w-full h-1/2 flex justify-center">
+              <van-icon :name="item.icon" />
+            </div>
+            <div class="w-full h-1/2 text-[10px] justify-center flex text-center">
+              {{ item.label }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <!-- 卡片B -->
     <div class="w-full h-28 flex absolute px-5 top-[286px] z-[19]">
-      <div class="w-full h-full info-card flex" />
+      <div class="w-full h-full info-card flex flex-col p-4">
+        <div class="w-full h-auto text-xl font-bold">更多服务</div>
+        <div class="w-full h-full grid grid-cols-4 gap-2">
+         <div class="w-full h-full flex flex-col">
+           <div class="w-full h-1/2 flex justify-center">
+             <van-icon name="chat-o" />
+           </div>
+           <div class="w-full h-1/2 flex justify-center text-[10px]">
+             用户反馈
+           </div>
+         </div>
+          <div class="w-full h-full flex flex-col">
+            <div class="w-full h-1/2 flex justify-center">
+              <van-icon name="service-o" />
+            </div>
+            <div class="w-full h-1/2 flex justify-center text-[10px]">
+              在线客服
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
